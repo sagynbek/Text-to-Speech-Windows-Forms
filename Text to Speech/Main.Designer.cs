5c6e73be1c61f63d96983cbb31fe13b59e5bc8f3
Sagynbek <kenzhebaevsagynbek@gmail.com>
2020-07-11 19:57:37 +0300

    maximize disabled

--- Text to Speech/Main.Designer.cs
+++ Text to Speech/Main.Designer.cs
@@ -297,6 +297,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
             this.Name = "Main";
             this.Text = "Main";
             this.Load += new System.EventHandler(this.Form1_Load);
